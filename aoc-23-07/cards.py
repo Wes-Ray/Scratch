d32064dcf6efc87268601918b11cac33eb63817a
Wes-Ray <42216488+Wes-Ray@users.noreply.github.com>
2023-12-07 00:46:13 -0600

    complete

--- aoc-23-07/cards.py
+++ aoc-23-07/cards.py
@@ -30,41 +30,55 @@ def hand_value(hand : str) -> int:
     hand = list(hand)
     hand.sort()
     # print("hand: ", hand)
-    
-    hand_type = 0
-    num_wilds = hand.count('J')
-    print("num_wilds: ", num_wilds)
-    # five of a kind = 6 pts
-    if hand.count(hand[0]) == 5:
-        hand_type = 6
-    # four of a kind = 5 pts
-    elif (hand.count(hand[0])+num_wilds == 4) or (hand.count(hand[-1])+num_wilds == 4):
-        hand_type = 5
-    # full house = 4 pts
-    elif hand.count(hand[0]) + hand.count(hand[-1]) == 5:
-        hand_type = 4
-    # three of a kind = 3 pts
-    elif (hand.count(hand[0]) == 3) or (hand.count(hand[-1]) == 3) or (hand.count(hand[1]) == 3):
-        hand_type = 3
-    
-    if hand_type == 0:
-        pair_count = 0
-        for ch in hand:
-            if hand.count(ch) == 2:
-                pair_count += 1
-        hand_type = pair_count // 2
-        
-        # print("\tpair count: ", pair_count)
-    
-    print(hand, end=" <- ")
 
-    strength = int(str(hand_type) + card_vals)
+    new_hands = []
+    for x in range(len(hand)):
+        # print(hand[x])
+        tmp_hand = hand
+        replace_char = hand[x]
+        tmp_hand = list(map(lambda x: x.replace('J', replace_char), tmp_hand))
+        tmp_hand.sort()
+        new_hands.append(tmp_hand)
+
+    # print(new_hands)
+    
+    max_hand_type = 0
+    for hand in new_hands:
+        hand_type = 0
+        # five of a kind = 6 pts
+        if hand.count(hand[0]) == 5:
+            hand_type = 6
+        # four of a kind = 5 pts
+        elif (hand.count(hand[0]) == 4) or (hand.count(hand[-1]) == 4):
+            hand_type = 5
+        # full house = 4 pts
+        elif hand.count(hand[0]) + hand.count(hand[-1]) == 5:
+            hand_type = 4
+        # three of a kind = 3 pts
+        elif (hand.count(hand[0]) == 3) or (hand.count(hand[-1]) == 3) or (hand.count(hand[1]) == 3):
+            hand_type = 3
+
+        if hand_type == 0:
+            pair_count = 0
+            for ch in hand:
+                if hand.count(ch) == 2:
+                    pair_count += 1
+            hand_type = pair_count // 2
+        
+        # print("hand_type: ", hand_type, end=" ")
+        
+        max_hand_type = max(max_hand_type, hand_type)
+    
+    # print(" max_hand_type: ", max_hand_type)
+        
+
+    strength = int(str(max_hand_type) + card_vals)
     # print("\tstrength: ", strength)
     return strength
 
 
 def main():
-    with open("ezinput2.txt", "r") as file:
+    with open("input.txt", "r") as file:
         hands = file.readlines()
     
     # for hand in hands:

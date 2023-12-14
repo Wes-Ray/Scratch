00841406df0ff5a1072953943e84a734a9593a9c
Wes <wesleyray95@gmail.com>
2023-12-14 11:01:25 -0600

    progress

--- aoc-23-14/rocks.py
+++ aoc-23-14/rocks.py
@@ -1,5 +1,35 @@
+from numpy import array
+import time
+
+class dir:
+    # (y, x)
+    EAST = (0, 1)
+    WEST = (0, -1)
+    NORTH = (-1, 0)
+    SOUTH = (1, 0)
+    CARDINALS = [NORTH, WEST, SOUTH, EAST]
 
 
+def tilt(matrix : array, tilt_dir : tuple) -> None:
+    print("tilt")
+
+    width = matrix[0].size  # width == height
+
+    # for i in range(width):  # repeats
+    #     for y in range(width-1, -1, -1):
+    #         # print(f"y: {y}")
+    #         for x in range(width):
+    #             # print(f"\tx: {x} {matrix[y][x]}")
+    #             if matrix[y][x] == 'O':
+    #                 if y-1 == -1:
+    #                     continue
+    #                 if matrix[y-1][x] == '.':
+    #                     matrix[y-1][x] = 'O'
+    #                     matrix[y][x] = '.'
+
+    # for y in range(width)
+
+    
 
 def part1(input : str):
     rows = []
@@ -7,23 +37,19 @@ def part1(input : str):
         lines = f.readlines()
         for l in lines:
             rows.append(list(l[:-1]))
-
-    for i in range(len(rows)):
-        for y in range(len(rows)-1, -1, -1):
-            # print(f"y: {y}")
-            for x in range(len(rows[0])):
-                # print(f"\tx: {x} {rows[y][x]}")
-                if rows[y][x] == 'O':
-                    if y-1 == -1:
-                        continue
-                    if rows[y-1][x] == '.':
-                        rows[y-1][x] = 'O'
-                        rows[y][x] = '.'
     
-    mult = len(rows)
+    matrix = array(rows)  # matrix[y][x]
+    # print(f"matrix: \n{matrix}")
+    
+    tilt(matrix, dir.NORTH)
+    
+    print("-"*20)
+    mult = matrix[0].size
     total = 0
-    for r in rows:
-        total += r.count('O') * mult
+    for r in matrix:
+        for v in r:
+            if v == 'O':
+                total += mult
         print("".join(r), mult)
         mult -= 1
     
@@ -32,7 +58,10 @@ def part1(input : str):
 
 def main():
     print("main")
-    part1("input.txt")
+    start = time.time()
+    part1("ezinput.txt")
+    end = time.time()
+    print(f"Elapsed Time: {end - start}")
 
 
 if __name__ == "__main__":
